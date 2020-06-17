89a1ed7056151bf0d5b3a43ab3c922c9feaa01fb
Enrique Santos <me@eos.fyi>
2020-06-17 01:58:12 -0700

    include necessary files

---
+++ nullDCNetplayLauncher/ControllerEngine.cs
@@ -0,0 +1,110 @@
+﻿using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace nullDCNetplayLauncher
+{
+    public class ControllerEngine : IDisposable
+    {
+        bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    //dispose managed resources
+                }
+            }
+            //dispose unmanaged resources
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            clock.Stop();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public DispatcherTimer clock;
+        private GamePadState oldgstate;
+
+        // Event Declaration
+        // GamePadAction: Triggered when the oldgstate is different from the current GamePadState
+        public event EventHandler<ActionEventArgs> GamePadAction;
+
+        private int clockspeed = 16;
+
+        public ControllerEngine()
+        {
+            this.ActiveDevice = 0;
+            oldgstate = GamePad.GetState(this.ActiveDevice);
+            createNewTimer();
+        }
+        private void createNewTimer()
+        {
+            clock = new DispatcherTimer();
+            clock.Tick += new EventHandler(checkGamePads);
+            clock.Interval = new TimeSpan(0, 0, 0, 0, this.clockspeed);
+            clock.Start();
+        }
+        public int ActiveDevice { get; set; }
+
+        public GamePadCapabilities CapabilitiesGamePad { get { return GamePad.GetCapabilities(this.ActiveDevice); } }
+
+        protected virtual void OnGamePadAction(ActionEventArgs e)
+        {
+            EventHandler<ActionEventArgs> handler = GamePadAction;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        private void checkGamePads(Object sender, EventArgs e)
+        {
+            ActionEventArgs args = new ActionEventArgs(this.ActiveDevice);
+            if (!args.GamePadState.Equals(oldgstate))
+            {
+                //call event
+                OnGamePadAction(args);
+                oldgstate = args.GamePadState;
+            }
+            if (!args.GamePadState.Triggers.Equals(oldgstate.Triggers))
+            {
+                //call event
+                OnGamePadAction(args);
+                oldgstate = args.GamePadState;
+            }
+
+        }
+
+    }
+
+    public class ActionEventArgs : EventArgs
+    {
+        public ActionEventArgs()
+        {
+            this.Instance = 0;
+        }
+
+        public ActionEventArgs(int ControllerInstance)
+        {
+            this.Instance = ControllerInstance;
+        }
+
+        public int Instance { get; set; }
+
+        public GamePadState GamePadState { get { return GamePad.GetState(this.Instance); } }
+
+    }
+}
---
+++ nullDCNetplayLauncher/GamePadMapping.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace nullDCNetplayLauncher
+{
+    public class GamePadMappingList
+    {
+        public List<GamePadMapping> GamePadMappings = new List<GamePadMapping>();
+    }
+    public class GamePadMapping
+    {
+        public string Name;
+
+        public string Y { get; set; } = "";
+        public string A { get; set; } = "";
+        public string Back { get; set; } = "";
+        public string X { get; set; } = "";
+        public string B { get; set; } = "";
+        public string Start { get; set; } = "";
+        public string LeftStick { get; set; } = "";
+        public string RightStick { get; set; } = "";
+        public string LeftShoulder { get; set; } = "";
+        public string RightShoulder { get; set; } = "";
+        public string LeftTrigger { get; set; } = "";
+        public string RightTrigger { get; set; } = "";
+        public string IsUp { get; set; } = "";
+        public string IsDown { get; set; } = "";
+        public string IsLeft { get; set; } = "";
+        public string IsRight { get; set; } = "";
+        public string BigButton { get; set; } = "";
+
+        public Boolean Default { get; set; } = false;
+
+        public object this[string propertyName]
+        {
+            get
+            {
+                if (propertyName.Equals("Left"))
+                    propertyName = "LeftTrigger";
+                else if(propertyName.Equals("Right"))
+                    propertyName = "RightTrigger";
+                System.Diagnostics.Debug.WriteLine(propertyName);
+                PropertyInfo property = GetType().GetProperty(propertyName);
+                return property.GetValue(this, null);
+            }
+            set
+            {
+                if (propertyName.Equals("Left"))
+                    propertyName = "LeftTrigger";
+                else if (propertyName.Equals("Right"))
+                    propertyName = "RightTrigger";
+                System.Diagnostics.Debug.WriteLine(propertyName);
+                PropertyInfo property = GetType().GetProperty(propertyName);
+                property.SetValue(this, value, null);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static GamePadMappingList ReadMappingsFile()
+        {
+            string path = Launcher.rootDir + "GamePadMappingList.xml";
+            GamePadMappingList readMappingList;
+            try
+            {
+                XmlSerializer serializer =
+                    new XmlSerializer(typeof(GamePadMappingList));
+                FileStream fs = new FileStream(path, FileMode.Open);
+
+                readMappingList = (GamePadMappingList)serializer.Deserialize(fs);
+                fs.Close();
+            }
+            catch (Exception)
+            {
+                GamePadMapping defaultMapping = new GamePadMapping
+                {
+                    Name = "Default",
+                    IsUp = "Up",
+                    IsDown = "Down",
+                    IsLeft = "Left",
+                    IsRight = "Right",
+                    A = "1",
+                    Y = "2",
+                    Back = "3",
+                    B = "4",
+                    X = "5",
+                    Start = "6",
+                    LeftStick = "Coin",
+                    RightStick = "Start",
+                    LeftShoulder = "",
+                    RightShoulder = "",
+                    LeftTrigger = "",
+                    RightTrigger = "",
+                    BigButton = "",
+                };
+                readMappingList = new GamePadMappingList();
+                readMappingList.GamePadMappings.Add(defaultMapping);
+
+                // generates a new file if it does not exist
+                System.Xml.Serialization.XmlSerializer serializer =
+                    new System.Xml.Serialization.XmlSerializer(typeof(GamePadMappingList));
+                StreamWriter writer = new StreamWriter(path);
+                serializer.Serialize(writer.BaseStream, readMappingList);
+                writer.Close();
+            }
+            return readMappingList;
+        }
+    }
+}
